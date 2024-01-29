98aa40b5af79a2160fa1b0d8866f9f9e5085ed2f
Sokolov Ivan <116334100+theruyk@users.noreply.github.com>
2024-01-29 19:43:44 +0600

    checkers.py

--- Progect_linux_curse/checkers.py
+++ Progect_linux_curse/checkers.py
@@ -30,16 +30,4 @@ def checkout(cmd: str, text: str, find_words_mode: bool = False) -> bool:
         # Стандартная проверка наличия заданного текста в выводе
         return text in result.stdout and result.returncode == 0
 
-def checkout_negative(cmd, text):
-    # Выполнение команды в оболочке и захват как стандартного вывода, так и стандартного потока ошибок
-    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
-    
-    # Проверка наличия заданного текста в выводе или потоке ошибок и неуспешного завершения команды (код возврата не 0)
-    if (text in result.stdout or text in result.stderr) and result.returncode != 0:
-        return True
-    else:
-        return False
 
-def getout(cmd):
-    # Выполнение команды и возвращение её вывода
-    return subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
