c503dd1714834548d523710d482580067dbef3e5
Иван Соколов <vano95.cokol@gmail.com>
2024-02-10 00:35:10 +0600

    done

--- Progect_linux_curse/conftest.py
+++ Progect_linux_curse/conftest.py
@@ -4,9 +4,10 @@ from checkers import checkout, getout
 import string
 import random
 import os
-import datetime
+from datetime import datetime
 import psutil
 import time
+from checkers_ssh import ssh_checkout,upload_files, ssh_getout
 
 @pytest.fixture(scope="session")
 def data():
@@ -19,17 +20,28 @@ def data():
 # Фикстура pytest для создания папок перед выполнением тестов
 @pytest.fixture(scope='class')
 def make_folders(data):
-    # Использует функцию checkout для выполнения команды mkdir и создания папок
-    return checkout(f"mkdir -p {data.get('folder_in')} {data.get('folder_out')} {data.get('folder_ext')} {data.get('folder_ext2')}", "")
+    # Использует функцию ssh_checkout для выполнения команды mkdir на удаленном сервере и создания папок
+    folder_in = data['folder_in']
+    folder_out = data['folder_out']
+    folder_ext = data['folder_ext']
+    folder_ext2 = data['folder_ext2']
+
+    mkdir_command = f"mkdir -p {folder_in} {folder_out} {folder_ext} {folder_ext2}"
+    
+    # Проверка успешности создания папок
+    ssh_result = ssh_checkout(data['ip'], data['username'], data['password'], mkdir_command, "created", data['port'])
+    
+    return ssh_result
 
 # Фикстура pytest для создания тестовых файлов
 @pytest.fixture(scope='class')
 def make_files(data):
     list_of_files = []
-    # Генерируем 5 случайных имен файлов и создаем их с помощью команды dd
+    # Генерируем 5 случайных имен файлов и создаем их с помощью команды dd на удаленном сервере
     for i in range(5):
         filename = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-        if checkout(f"cd {data.get('folder_in')}; dd if=/dev/urandom of={filename} bs=1M count=1 iflag=fullblock", ""):
+        dd_command = f"dd if=/dev/urandom of={data.get('folder_in')}/{filename} bs=1M count=1 iflag=fullblock"
+        if ssh_checkout(data['ip'], data['username'], data['password'], dd_command, "bytes copied", data['port']):
             list_of_files.append(filename)
     return list_of_files
 
@@ -39,36 +51,47 @@ def make_subfolder(data):
     # Генерируем случайное имя для тестового файла и подпапки
     testfilename = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
     subfoldername = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-    # Создаем подпапку
-    if not checkout(f"cd {data.get('folder_in')}; mkdir {subfoldername}", ""):
+    # Создаем подпапку на удаленном сервере
+    mkdir_command = f"mkdir -p {data.get('folder_in')}/{subfoldername}"
+    if not ssh_checkout(data['ip'], data['username'], data['password'], mkdir_command, "created", data['port']):
         return None, None
-    # Создаем тестовый файл
-    if not checkout(f"cd {data.get('folder_in')}/{subfoldername}; dd if=/dev/urandom of={testfilename} bs=1M count=1 iflag=fullblock", ""):
+    # Создаем тестовый файл на удаленном сервере
+    dd_command = f"dd if=/dev/urandom of={data.get('folder_in')}/{subfoldername}/{testfilename} bs=1M count=1 iflag=fullblock"
+    if not ssh_checkout(data['ip'], data['username'], data['password'], dd_command, "1+0 records in", data['port']):
         return subfoldername, None
-    else:
-        return subfoldername, testfilename
+    return subfoldername, testfilename
         
 # Фикстура для очистки папок перед тестом
 @pytest.fixture(scope='class')
 def clear_folders(data):
-    # Используется функция checkout для удаления содержимого папок
-    yield
-    return checkout(f"rm -rf folder_in/* folder_out/* folder_ext/* folder_ext2/*", "")
+    # Очистка папок перед началом теста
+    yield  # Это позволяет сначала выполнить тесты, а затем очистить папки после их завершения
 
-@pytest.fixture(autouse=True)
-def stats_collector(request, data):
-    def finalizer():
-        time_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file_count = len(os.listdir(data['folder_in']))
-        file_sizes = sum(os.path.getsize(os.path.join(data['folder_in'], f)) for f in os.listdir(data['folder_in']))
+    # Команда для удаления содержимого папок на удаленном сервере
+    clear_command = f"rm -rf {data.get('folder_in')}/* {data.get('folder_out')}/* {data.get('folder_ext')}/* {data.get('folder_ext2')}/*"
+    
+    # Выполнение команды удаления
+    return ssh_checkout(data['ip'], data['username'], data['password'], clear_command, "", data['port'])
+
+
+
+# @pytest.fixture(autouse=True)
+# def stats_collector(request, data):
+#     def finalizer():
+#         time_now =datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#         file_count = len(os.listdir(data['folder_in']))
+#         file_sizes = sum(os.path.getsize(os.path.join(data['folder_in'], f)) for f in os.listdir(data['folder_in']))
         
-        # Первый вызов для инициализации измерения
-        psutil.cpu_percent(interval=None)
-        time.sleep(1)  # Небольшая задержка
-        cpu_load = psutil.cpu_percent(interval=None)  # Второй вызов для получения актуального значения загрузки ЦПУ
+#         # Первый вызов для инициализации измерения
+#         psutil.cpu_percent(interval=None)
+#         time.sleep(1)  # Небольшая задержка
+#         cpu_load = psutil.cpu_percent(interval=None)  # Второй вызов для получения актуального значения загрузки ЦПУ
 
-        with open('stat.txt', 'a') as stat_file:
-            stat_file.write(f"{time_now}, {file_count}, {file_sizes}, {cpu_load}\n")
+#         with open('stat.txt', 'a') as stat_file:
+#             stat_file.write(f"{time_now}, {file_count}, {file_sizes}, {cpu_load}\n")
 
-    request.addfinalizer(finalizer)
+#     request.addfinalizer(finalizer)
 
+@pytest.fixture()
+def time_now():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
--- Progect_linux_curse/progect.py
+++ Progect_linux_curse/progect.py
@@ -1,78 +1,200 @@
 from checkers import checkout, getout
 import yaml
 import pytest
+from checkers_ssh import ssh_checkout,upload_files, ssh_getout
+import datetime
+import subprocess
 
-@pytest.mark.usefixtures('make_folders','make_files','make_subfolder','clear_folders','stats_collector')
+@pytest.mark.usefixtures('make_folders','make_files','make_subfolder','clear_folders')#'stats_collector'
 class TestSevenZ:
 
-  def test_step1(self, data):
+  def save_log(self, data, time_now, file_name):
+      # Команда для получения логов с удаленного сервера
+      command = f"journalctl --since '{time_now}'"
+      
+      # Получаем вывод команды с удаленного сервера с помощью функции ssh_getout
+      log_output = ssh_getout(data['ip'], data['username'], data['password'], command, data['port'])
+      
+      # Записываем полученный вывод в файл
+      with open(file_name, "w") as f:
+          f.write(log_output)
+
+
+
+  def test_step0(self, time_now, data):
+    # Получение данных из конфигурации
+    remote_ip = data['ip']
+    username = data['username']
+    password = data['password']
+    install_path = data['install_path']
+    local_path = data['local_path']
+    port = data['port']
+
+    # Загружает файл на удаленный сервер и выполняет команды по установке пакета.
+    res = []
+    upload_files(remote_ip, username, password, local_path, install_path + "p7zip-full.deb", port=port)
+    command_install = f"echo '{password}' | sudo -S dpkg -i {install_path}p7zip-full.deb"
+    command_check = f"echo '{password}' | sudo -S dpkg -s p7zip-full"
+    install_message = "Setting up"
+    check_message = "Status: install ok installed"
+    res.append(ssh_checkout(remote_ip, username, password, command_install, install_message))
+    res.append(ssh_checkout(remote_ip, username, password, command_check, check_message))
+    self.save_log(data,time_now, 'step0.txt')
+    assert all(res)
+
+  def test_step1(self, data,time_now):
     # Тест 1: Создание архива из содержимого папки folder_in.
+    archive_type = data['archive_type']
+    folder_in = data['folder_in']
+    folder_out = data['folder_out']
+    host = data['ip']
+    user = data['username']
+    passwd = data['password']
+    port = data['port']
     archive_type = data.get('archive_type')  # Получаем тип архива из конфигурации
-    res1 = checkout("cd {}; 7z a {} {}/arx2 *".format(data.get('folder_in'), archive_type, data.get('folder_out')), "Everything is Ok")
-    res2 = checkout("ls {}".format(data.get('folder_out')), "arx2.7z")
+    command_create_archive = f"cd {folder_in}; 7z a {archive_type} {folder_out}/arx2 *"
+    command_check_archive = f"ls {folder_out}"
+    res1 = ssh_checkout(host, user, passwd, command_create_archive, "Everything is Ok", port)
+    res2 = ssh_checkout(host, user, passwd, command_check_archive, "arx2.7z", port)
+    self.save_log(data,time_now, 'step1')
     assert res1 and res2, "test1 FAIL"
 
-  def test_step2(self,make_files,data):
-      # Тест 2: Проверка наличия файлов внутри архива arx2.7z.
+  def test_step2(self, make_files, data, time_now):
+      # Тест 2: Проверка наличия файлов внутри архива arx2.7z на удаленном сервере.
       res = []
-      res.append(checkout("cd {}; 7z a {}/arx2".format(data.get('folder_in'), data.get('folder_out')), "Everything is Ok"))
-      res.append(checkout("cd {}; 7z e arx2.7z -o{} -y".format(data.get('folder_out'), data.get('folder_ext')), "Everything is Ok"))
+      archive_command = f"cd {data['folder_in']}; 7z a {data['folder_out']}/arx2 *"
+      archive_result = ssh_checkout(data['ip'], data['username'], data['password'], archive_command, "Everything is Ok", data['port'])
+      res.append(archive_result)
+
+      extract_command = f"cd {data['folder_out']}; 7z e arx2.7z -o{data['folder_ext']} -y"
+      extract_result = ssh_checkout(data['ip'], data['username'], data['password'], extract_command, "Everything is Ok", data['port'])
+      res.append(extract_result)
+      
+      # Проверка наличия каждого файла в распакованной директории
       for item in make_files:
-          res.append(checkout("ls {}".format(data.get('folder_ext')), item))
+          check_file_command = f"ls {data['folder_ext']}"
+          res.append(ssh_checkout(data['ip'], data['username'], data['password'], check_file_command, item, data['port']))
+      
+      self.save_log(data,time_now, 'step2')
       assert all(res), "test2 FAIL"
 
-  def test_step3(self,data):
-      # Тест 3: Проверка целостности архива
-      assert checkout(f"cd {data.get('folder_out')}; 7z t arx2.7z", "Everything is Ok"), "test3 FAIL"
 
-  def test_step4(self,data):
-      # Тест 4: Проверка обновления содержимого архива
-      assert checkout(f"cd {data.get('folder_in')}; 7z u arx2.7z", "Everything is Ok"), "test4 FAIL"
+  def test_step3(self, data, time_now):
+    # Тест 3: Проверка целостности архива на удаленном сервере.
+    check_archive_command = f"cd {data['folder_out']}; 7z t arx2.7z"
+    res = ssh_checkout(data['ip'], data['username'], data['password'], check_archive_command, "Everything is Ok", data['port'])
+    
+    self.save_log(data,time_now, 'step3')
+    assert res, "test3 FAIL"
 
-  def test_step5(self, make_files, data):
-      # Тест 5: Проверка создания архива и его содержимого
+  def test_step4(self, data, time_now):
+    # Тест 4: Проверка обновления содержимого архива на удаленном сервере.
+    update_command = f"cd {data['folder_in']}; 7z u {data['folder_out']}/arx2.7z"
+    res = ssh_checkout(data['ip'], data['username'], data['password'], update_command, "Everything is Ok", data['port'])
+
+    self.save_log(data,time_now, 'step4')
+    assert res, "test4 FAIL"
+
+  def test_step5(self, make_files, data, time_now):
+      # Тест 5: Проверка создания архива и его содержимого на удаленном сервере.
       res = []
-      archive_type = data.get('archive_type')  # Получаем тип архива из конфигурации
-      # Создает архив и проверяет, было ли это успешно
-      res.append(checkout(f"cd {data.get('folder_in')}; 7z a {archive_type} {data.get('folder_out')}/arx2", "Everything is Ok"))
-      # Проверяет наличие каждого файла из списка make_files в архиве arx2.7z
+      archive_command = f"cd {data['folder_in']}; 7z a {data['archive_type']} {data['folder_out']}/arx2 *"
+      res.append(ssh_checkout(data['ip'], data['username'], data['password'], archive_command, "Everything is Ok", data['port']))
+
+      # Получение списка файлов в архиве
+      list_command = f"cd {data['folder_out']}; 7z l arx2.7z"
+      list_output = ssh_getout(data['ip'], data['username'], data['password'], list_command, data['port'])
       for item in make_files:
-          res.append(checkout(f"cd {data.get('folder_out')}; 7z l arx2.7z", item))
-      # Проверяет, что все предыдущие действия прошли успешно
+          if item in list_output:
+              res.append(True)
+          else:
+              res.append(False)
+
+      self.save_log(data,time_now, 'step5')
       assert all(res), "test5 FAIL"
 
 
-  def test_step6(self, make_files, data):
-      # Тест 6: Проверка содержимого архива и извлечения файлов
-      res = []
-      archive_type = data.get('archive_type')  # Получаем тип архива из конфигурации
-      # Создает архив и проверяет, было ли это успешно
-      res.append(checkout(f"cd {data.get('folder_in')}; 7z a {archive_type} {data.get('folder_out')}/arx", "Everything is Ok"))
-      # Извлекает содержимое архива и проверяет, было ли это успешно
-      res.append(checkout(f"cd {data.get('folder_out')}; 7z x arx.7z -o{data.get('folder_ext2')} -y", "Everything is Ok"))
-      # Проверяет наличие каждого файла из списка make_files в извлеченной папке folder_ext2
-      for item in make_files:
-        res.append(checkout(f"ls {data.get('folder_ext2')}", item))
-      # Проверяет, что все предыдущие действия прошли успешно
-      assert all(res), "test6 FAIL"
 
 
-  def test_step7(self,data):
-      # Тест 7: Проверка удаления содержимого архива
-      assert checkout(f"cd {data.get('folder_out')}; 7z d arx.7z", "Everything is Ok"), "test7 FAIL"
+  def test_step6(self, make_files, data, time_now):
+    # Тест 6: Проверка содержимого архива и извлечения файлов на удаленном сервере.
+    res = []
+    create_archive_command = f"cd {data['folder_in']}; 7z a {data['archive_type']} {data['folder_out']}/arx *"
+    extract_command = f"cd {data['folder_out']}; 7z x arx.7z -o{data['folder_ext2']} -y"
+    
+    # Выполнение команд на удаленном сервере
+    res.append(ssh_checkout(data['ip'], data['username'], data['password'], create_archive_command, "Everything is Ok", data['port']))
+    res.append(ssh_checkout(data['ip'], data['username'], data['password'], extract_command, "Everything is Ok", data['port']))
 
-  def test_step8(self,make_files,data):
+    # Получение списка файлов в извлеченной папке
+    list_files_command = f"ls {data['folder_ext2']}"
+    list_files_output = ssh_getout(data['ip'], data['username'], data['password'], list_files_command, data['port'])
+    extracted_files = list_files_output.split()
+
+    # Проверка наличия каждого файла из списка make_files в извлеченной папке
+    for expected_file in make_files:
+        if expected_file in extracted_files:
+            res.append(True)
+        else:
+            res.append(False)
+
+    self.save_log(data,time_now, 'step6')
+    assert all(res), "test6 FAIL"
+
+
+  def test_step7(self, data, time_now):
+    # Тест 7: Проверка удаления содержимого архива на удаленном сервере.
+    remove_command = f"cd {data['folder_out']}; 7z d arx.7z"
+    res = ssh_checkout(data['ip'], data['username'], data['password'], remove_command, "Everything is Ok", data['port'])
+
+    self.save_log(data,time_now, 'step7')
+    assert res, "test7 FAIL"
+
+
+  def test_step8(self, make_files, data, time_now):
       # Тест 8: Проверка расчета хеша файлов и сравнение с выводом команды crc32
       res = []
       for item in make_files:
-          # Выполняет команду для расчета хеша файла с помощью 7z и добавляет результат в список res
-          res.append(checkout(f"cd {data.get('folder_in')}; 7z h {item}", "Everything is Ok"))
-          # Вызывает команду crc32 для расчета хеша и преобразует вывод в верхний регистр
-          hash = getout(f"cd {data.get('folder_in')}; crc32 {item}").upper()
-          # Выполняет команду для проверки хеша файла с помощью 7z и добавляет результат в список res
-          res.append(checkout(f"cd {data.get('folder_in')}; 7z h {item}", hash))
-      # Проверяет, все ли результаты в res истинны (True)
+          # Выполнение команды для расчета хеша файла с помощью 7z
+          hash_command = f"cd {data['folder_in']}; 7z h {item}"
+          hash_command_output = ssh_getout(data['ip'], data['username'], data['password'], hash_command, data['port'])
+          
+          # Получение хеша командой crc32 и преобразование его в верхний регистр
+          crc32_command = f"crc32 {data['folder_in']}/{item}"
+          crc32_hash = ssh_getout(data['ip'], data['username'], data['password'], crc32_command, data['port']).strip().upper()
+
+          # Сравнение хеша из 7z и crc32
+          if crc32_hash in hash_command_output:
+              res.append(True)
+          else:
+              res.append(False)
+
+      self.save_log(data,time_now, 'step8')
       assert all(res), "test8 FAIL"
+
+
+  def test_step9(self, time_now, data):
+      # Удаляет файл с удаленного сервера и выполняет команды по удалению пакета.
+      res = []
+      # Получение данных из конфигурации
+      remote_ip = data['ip']
+      username = data['username']
+      password = data['password']
+      remote_path = data['install_path']
+      local_path = data['local_path']
+      port = data['port']
+
+      upload_files(remote_ip, username, password, local_path, f"{remote_path}/p7zip-full.deb", port=port)
+      remove_command = f"echo '{password}' | sudo -S dpkg -r p7zip-full"
+      command_check = f"echo '{password}' | sudo -S dpkg -s p7zip-full"
+      remove_message = "Removing"
+      check_message = "Status: deinstall ok"
+      res.append(ssh_checkout(remote_ip, username, password, remove_command, remove_message))
+      res.append(ssh_checkout(remote_ip, username, password, command_check, check_message))
+      self.save_log(data,time_now, 'step9')
+      assert all(res)
+
+
 
 # if __name__ == '__main__':
 #  pytest.main['-vv']
